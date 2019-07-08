da151fd49452c1314c868207200976b548391902
paulo matsinhe <psmatsinhe@gmail.com>
2019-07-08 13:14:04 +0200

    changing the way csv is generated

--- metadata_finds/metadata_finds.sh
+++ metadata_finds/metadata_finds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#This  script runs  in all pattners databases in order  to find nonconformity patient 
-# and  displays in in *.csv  file
+#This  script runs  in all pattners databases in order  to find all metadata 
+#and  displays in in *.csv  file
 
 
 #Collecting MySQL credential
@@ -68,7 +68,7 @@ if [ $all_db_anserwer == "y" ]
                                 unset db_list[$index]            
                         fi 
                         let index++
-        done
+                done
 
 elif [ $all_db_anserwer == "n" ];
 	then
@@ -103,50 +103,69 @@ declare -A queries
 
 ####  the bellow  queries  will used to run in mentioned partners databases  ####
 #--Get List of all programs
-queries[0,0]="SELECT program_id,name,retired program_retired,uuid FROM program ORDER BY program_id;"
-queries[0,1]="all programs"
+queries[0,0]="SELECT program_id,name,retired program_retired,uuid FROM program ORDER BY program_id"
+queries[0,1]="all-programs"
 
 #--Get all the workflow of the programs
-queries[1,0]="SELECT p.program_workflow_id,p.program_id,p.retired program_workflow_retired, p.concept_id,c.name,p.uuid program_workflow_uuid,c.uuid concept_uuid FROM program_workflow p INNER JOIN concept_name c ON p.concept_id=c.concept_id ORDER BY program_workflow_id;"
-queries[1,1]="all workflow"
+queries[1,0]="SELECT p.program_workflow_id,p.program_id,p.retired program_workflow_retired, p.concept_id,c.name,p.uuid program_workflow_uuid,c.uuid concept_uuid FROM program_workflow p INNER JOIN concept_name c ON p.concept_id=c.concept_id ORDER BY program_workflow_id"
+queries[1,1]="all-workflow"
 
 #--Get all the workflow states
-queries[2,0]="SELECT p.program_workflow_id,p.program_Workflow_state_id,p.retired program_workflow_state_retired, p.concept_id, c.name, p.uuid program_workflow_state_uuid FROM program_workflow_state p INNER JOIN concept_name c ON p.concept_id=c.concept_id ORDER BY program_workflow_id;"
-queries[2,1]="all workflow states"
+queries[2,0]="SELECT p.program_workflow_id,p.program_Workflow_state_id,p.retired program_workflow_state_retired, p.concept_id, c.name, p.uuid program_workflow_state_uuid FROM program_workflow_state p INNER JOIN concept_name c ON p.concept_id=c.concept_id ORDER BY program_workflow_id"
+queries[2,1]="all-workflow-states"
 
 #--Get all the person attribute types
-queries[3,0]="SELECT person_attribute_type_id, name,retired,uuid FROM  person_attribute_type ORDER BY person_attribute_type_id;"
-queries[3,1]="all person attributes"
+queries[3,0]="SELECT person_attribute_type_id, name,retired,uuid FROM  person_attribute_type ORDER BY person_attribute_type_id"
+queries[3,1]="all-person-attributes"
 
 #--Get all the encounter type
-queries[4,0]="SELECT encounter_type_id, name, retired,uuid FROM encounter_type ORDER BY encounter_type_id;"
-queries[4,1]="all encounter types"
+queries[4,0]="SELECT encounter_type_id, name, retired,uuid FROM encounter_type ORDER BY encounter_type_id"
+queries[4,1]="all-encounter-types"
 
 #--Get all the forms
-queries[5,0]="SELECT form_id, name,retired,uuid FROM form;"
-queries[5,1]="all forms"
+queries[5,0]="SELECT form_id, name,retired,uuid FROM form"
+queries[5,1]="all-forms"
 
 #--Get all patient identifier type
-queries[6,0]="SELECT patient_identifier_type_id, name,retired,uuid FROM  patient_identifier_type;"
-queries[6,1]="all identifier types"
+queries[6,0]="SELECT patient_identifier_type_id, name,retired,uuid FROM  patient_identifier_type"
+queries[6,1]="all-identifier-types"
 
 #--Get all order types
-queries[7,0]="SELECT order_type_id,name,retired,uuid FROM order_type;"
-queries[7,1]="all order types"
+queries[7,0]="SELECT order_type_id,name,retired,uuid FROM order_type"
+queries[7,1]="all-order-types"
 
 #--Get all user roles
-queries[8,0]="SELECT role,uuid FROM role ORDER BY role;"
-queries[8,1]="all roles"
+queries[8,0]="SELECT role,uuid FROM role ORDER BY role"
+queries[8,1]="all-roles"
 
 #--Get all visit types
-queries[9,0]="SELECT visit_type_id,name,retired,uuid FROM visit_type ORDER BY visit_type_id;"
-queries[9,1]="all visit types"
+queries[9,0]="SELECT visit_type_id,name,retired,uuid FROM visit_type ORDER BY visit_type_id"
+queries[9,1]="all-visit-types"
 
 #--Get all forms not being used in any encounter
-queries[10,0]="SELECT form_id,name,retired,uuid FROM form WHERE form_id NOT IN (SELECT DISTINCT form_id FROM encounter);"
-queries[10,1]="all forms not in encounter"
+queries[10,0]="SELECT form_id,name,retired,uuid FROM form WHERE form_id NOT IN (SELECT DISTINCT form_id FROM encounter)"
+queries[10,1]="all-forms-not-in-encounter"
 
-query=""
+
+#Get Mysql files writing directory
+#Files will be written in this directory then copied to ~/epts_metadata_finds
+mysql -u $mysql_user -p$mysql_passwd -h $host -e "SHOW VARIABLES LIKE 'secure_file_priv';" >  temp.txt
+file="temp.txt"
+i=1
+while IFS= read -r line;
+do        
+        if [[ $i == 0 ]];
+        then
+                echo "Adding mysql path..."
+                array=($line)  
+                mysqlPath=${array[1]}                                      
+                echo "Mysql file path ${array[1]}"
+        fi
+        let i--
+done < "$file"
+
+#Remove all the content of mysql file path before start creating new files
+rm -rf $mysqlPath*
 #change this funtion to use a loop by creating the queries as arrays.
 #connecting to  MySQL instance
 echo "Connecting  to your mysql instance................................................"
@@ -154,18 +173,28 @@ for  db in "${db_list[@]}"
 	do
         echo "   " 
         echo "##################################  perfoming in $db database............................................" 
-            mkdir ~/epts_metadata_finds/"$db"
+            #mkdir ~/epts_metadata_finds/"$db"
+            path=$mysqlPath"$db"
+            mkdir $path
+            #Give the ownership to mysql cause if not will not be able to right in the folder
+            chown -R mysql:mysql $path
 
-        for((i=0;i<=11;i++));
+        for((i=0;i<=10;i++));
         do
              echo "${queries[$i,1]}"
-             mysql -u $mysql_user -p$mysql_passwd -h $host -D$db -e "${queries[$i,0]}" >  ~/epts_metadata_finds/"$db"/"$db_${queries[$i,1]}.csv"
+             sql="${queries[$i,0]} INTO OUTFILE '$path/$db_${queries[$i,1]}.csv';"
+             echo " Here is the query: $sql"
+             mysql -u $mysql_user -p$mysql_passwd -h $host -D$db -e "$sql"
+             #mysql -u $mysql_user -p$mysql_passwd -h $host -D$db -e "${queries[$i,0]}" >  ~/epts_metadata_finds/"$db"/"$db_${queries[$i,1]}.csv"
         done 
         
 	echo "concluded in $db...................................................."
             
 done
 
+#moving now all the files to ~/epts_metadata_finds
+cp -v -r $mysqlPath* ~/epts_metadata_finds/
+ls ~/epts_metadata_finds/
 echo "   "
 echo "Search in Data Base(s) completed ........................................  "
 echo "..........................."
